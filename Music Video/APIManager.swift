4b6a42be0add41c632be009e1a7bb833712356e5
Emir haktan Ozturk <haktan_emir@yahoo.com>
2016-03-13 18:00:05 +0100

    Json Serialization : converting incoming data to Json

--- Music Video/APIManager.swift
+++ Music Video/APIManager.swift
@@ -9,30 +9,45 @@
 import Foundation
 
 class APIManager {
+    
     func loadData(urlString:String, completion:(result:String) -> Void){
     
-        
         let config = NSURLSessionConfiguration.ephemeralSessionConfiguration()
         
         let session = NSURLSession(configuration: config)
         
+        let url = NSURL(string: urlString)!  // url i al
         
-        
-        //let session = NSURLSession.sharedSession()
-        let url = NSURL(string: urlString)!
-        
-        let task = session.dataTaskWithURL(url){
+        let task = session.dataTaskWithURL(url) { // url ile task oluştur içinde data, response ve error olsun
         (data, response, error) -> Void in
-        
-        dispatch_async(dispatch_get_main_queue()){
-            if (error != nil) {
+            
+            if error != nil {
+                dispatch_async(dispatch_get_main_queue()){  // hata internet bağlantı hatası
                 completion(result: (error!.localizedDescription))
-            }else{
-            completion(result: "NSURLSession Successful!")
-            print(data)
+                }
+            } else{
+                print(data) // data yı log da göster
+                /* 
+                aşağıda daha önce getirilen NSData Json verisine çevriliyor bunun için do catch bloğu kullanılıyor
+                */
+                do{
+                    if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+                        as?[String: AnyObject]{//gelen objeyi json serialization yap
+                        print(json)
+                            let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                            dispatch_async(dispatch_get_global_queue(priority, 0)){ //global queue
+                                dispatch_async(dispatch_get_main_queue()){
+                                completion(result: "JSon Serialization Succesfull")
+                                }
+                            }
+                    }
+                }catch{
+                    dispatch_async(dispatch_get_main_queue()){
+                        completion(result: "error in Json Serialization")
+                    }
+                }
             }
         }
-    }
-    task.resume()
+        task.resume()
     }
 }
