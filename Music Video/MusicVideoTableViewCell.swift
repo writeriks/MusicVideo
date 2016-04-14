2babd4aae75e656bfc13be2a671b25d5a8c04efc
Emir haktan Ozturk <haktan_emir@yahoo.com>
2016-04-14 17:18:44 +0200

    Application Transport Block edited and images are visible

--- Music Video/MusicVideoTableViewCell.swift
+++ Music Video/MusicVideoTableViewCell.swift
@@ -28,6 +28,36 @@ class MusicVideoTableViewCell: UITableViewCell {
     func updateCell(){
         musicTitle.text = video?.vName
         rank.text = ("\(video!.vRank)") // toString()
-        musicImage.image = UIImage(named: "imageNotAvailable")
+        
+        //musicImage.image = UIImage(named: "imageNotAvailable")
+        
+        if video!.vImageData != nil {
+            print("getting data from array")
+            musicImage.image = UIImage(data: video!.vImageData!)
+        }else{
+          getVideoImage(video!, imageView: musicImage)
+        }
     }
+    
+    func getVideoImage(video:Videos, imageView : UIImageView){
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)){
+
+            let data = NSData(contentsOfURL: NSURL(string: video.vImageUrl)!)
+            
+            var image : UIImage?
+            
+            if data != nil{
+                video.vImageData = data
+                image = UIImage(data: data!)
+            }
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                imageView.image = image
+            }
+        }
+
+    }
+    
+    
 }
