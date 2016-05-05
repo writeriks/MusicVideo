130cc8536bf579357118fcd857664fb6248feebf
Emir haktan Ozturk <haktan_emir@yahoo.com>
2016-05-05 17:03:45 +0200

    sec switch and slider added

--- Music Video/SettingsTVC.swift
+++ Music Video/SettingsTVC.swift
@@ -12,7 +12,6 @@ class SettingsTVC: UITableViewController {
 
     @IBOutlet var aboutDisplay: UILabel!
     @IBOutlet var feedbackDisplay: UILabel!
-
     @IBOutlet var securityDisplay: UILabel!
     @IBOutlet var touchID: UISwitch!
     @IBOutlet var bestImageQualityDisplay: UILabel!
@@ -20,15 +19,47 @@ class SettingsTVC: UITableViewController {
     @IBOutlet var sliderCnt: UISlider!
 
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Settings"
         tableView.separatorStyle = .None
+        
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "prefferedFontChange", name: UIContentSizeCategoryDidChangeNotification, object: nil)
 
         tableView.alwaysBounceVertical = false
+        
+        touchID.on = NSUserDefaults.standardUserDefaults().boolForKey("SecSetting")
+        
+        if(NSUserDefaults.standardUserDefaults().objectForKey("APICNT") != nil){
+            
+            let theValue = NSUserDefaults.standardUserDefaults().objectForKey("APICNT") as! Int
+            APICnt.text = "\(theValue)"
+            sliderCnt.value = Float(theValue)
+            
+        }
+        
     }
+   
+    @IBAction func touchIdSec(sender: UISwitch) {
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        if touchID.on{
+            defaults.setBool(touchID.on, forKey: "SecSetting")
+        }
+        else{
+            defaults.setBool(false, forKey: "SecSetting")
+        }
+        
+    }
+
+    @IBAction func valueChanged(sender: AnyObject) {// slider slide change to apiCnt number
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(Int(sliderCnt.value), forKey: "APICNT") // slider value type is float so change it to int
+        APICnt.text = ("\(Int(sliderCnt.value))")  //ToString()
+        
+    }
+    
     
     func prefferedFontChange(){
         aboutDisplay.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
